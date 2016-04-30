1157ec35ae300fe995bc2326100529a50e1090e9
LENYLANE <ALENA@KRUCREW.COM>
2016-04-30 14:26:07 -0700

    Add all app_routes

--- controllers/server.py
+++ controllers/server.py
@@ -4,10 +4,45 @@ server = Blueprint('server', __name__, template_folder='views')
 
 @server.route('/')
 def main_route():
+	"""Show landing page"""
+
     options = {}
     return render_template("index.html", **options)
 
+
 @server.route('/signup')
 def signup_route():
+	"""Sign up page"""
+
+	# 2.0 log in option
+
 	options = {}
 	return render_template("signup_form.html", **options)
+
+
+@server.route('/home')
+def home_rout():
+	"""Homepage with search form"""
+
+	# 2.0 search options for geo proximity, last update
+
+	options = {}
+	return render_template("home.html", **options)
+
+
+@server.route('/view_resources')
+def view_resources():
+	"""List resources based on db query take action"""
+
+	options = {}
+	return render_template("resources.html", **options)
+
+
+@server.route('/org/<int:uniq_id>')
+def view_org():
+	"""View org info. Option to contact orgfrom page."""
+
+	options = {}
+	return render_template("org.html", **options)
+
+
